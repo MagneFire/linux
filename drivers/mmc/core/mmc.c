966d63b7ecbc67c85cb4d69e3072ab083911b2de
Darrel Griët <dgriet@gmail.com>
2023-06-01 22:49:32 +0200

    Wait for mmc to be ready.
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -2990,6 +2990,9 @@ static const struct mmc_bus_ops mmc_ops = {
 	.change_bus_speed = mmc_change_bus_speed,
 };
 
+static void ClearMMC_Ready(void);
+static void SetMMC_Ready(void);
+
 /*
  * Starting point for MMC card init.
  */
@@ -3001,6 +3004,7 @@ int mmc_attach_mmc(struct mmc_host *host)
 	BUG_ON(!host);
 	WARN_ON(!host->claimed);
 
+	ClearMMC_Ready();
 	/* Set correct bus mode for MMC before attempting attach */
 	if (!mmc_host_is_spi(host))
 		mmc_set_bus_mode(host, MMC_BUSMODE_OPENDRAIN);
@@ -3049,6 +3053,8 @@ int mmc_attach_mmc(struct mmc_host *host)
 	if (err)
 		goto remove_card;
 
+	msleep(80); // Wait MMC driver and partition are ready.
+	SetMMC_Ready();
 	register_reboot_notifier(&host->card->reboot_notify);
 
 	return 0;
@@ -3065,4 +3071,20 @@ err:
 		mmc_hostname(host), err);
 
 	return err;
+}
+
+static bool gMMC_Ready=0;
+static void ClearMMC_Ready(void)
+{
+	gMMC_Ready = 0;
+}
+
+static void SetMMC_Ready(void)
+{
+	gMMC_Ready = 1;
+}
+
+bool GetMMC_Ready(void)
+{
+	return gMMC_Ready;
 }
